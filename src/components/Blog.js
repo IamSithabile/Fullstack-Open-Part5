1c3a7d969017847b29191ddd99f91b957cc7963d
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-23 22:02:21 +0200

    Exercise 5.7 step 7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,53 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const { title, url, author, likes } = blog;
+
+  const [show, setShow] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  if (!show) {
+    return (
+      <div style={blogStyle}>
+        <p>{title}</p>
+        <p>{author}</p>
+        <button
+          onClick={() => {
+            setShow(true);
+          }}
+        >
+          View
+        </button>
+      </div>
+    );
+  }
+  return (
+    <>
+      <div style={blogStyle}>
+        <p>{title}</p>
+        <p>{url}</p>
+        <p>
+          {likes}
+          <button>like</button>
+        </p>
+        <p>{author}</p>
+      </div>
+      <button
+        onClick={() => {
+          setShow(false);
+        }}
+      >
+        Hide
+      </button>
+    </>
+  );
+};
 
 export default Blog;
