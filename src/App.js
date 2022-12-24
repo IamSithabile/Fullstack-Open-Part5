0c3da1407db94f6b29325ab15c5637506563c30c
IamSithabile <91020386+IamSithabile@users.noreply.github.com>
2022-12-24 14:30:09 +0200

    Exercise 5.12 step 12

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
   const sortByLikes = (a, b) => {
     if (a.likes > b.likes) {
       return -1;
-    } else if (a.likes > b.likes) {
+    } else if (a.likes < b.likes) {
       return 1;
     } else {
       return 0;
